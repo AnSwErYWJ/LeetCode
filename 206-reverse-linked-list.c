22c8b453ee5f2da90fa127377ecfe763100f0aa3
answerywj <1045837697@qq.com>
2022-04-11 21:02:41 +0800

    update 206-reverse-linked-lis

--- 206-reverse-linked-list.c
+++ 206-reverse-linked-list.c
@@ -6,15 +6,34 @@
  * };
  */
 
+#if 1
+// 迭代
 struct ListNode* reverseList(struct ListNode* head){
     struct ListNode *cur = head;
     struct ListNode *pre = NULL;
+    struct ListNode *next = NULL;
 
     while(cur != NULL) {
-        struct ListNode *next = cur->next;
+        next = cur->next;
         cur->next = pre;
         pre = cur;
         cur = next;
     }
     return pre;
 }
+
+#else
+// 递归
+struct ListNode* reverseList(struct ListNode* head){
+    if (NULL == head || NULL == head->next) {
+        return head;
+    }
+
+    struct ListNode *new = reverseList(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    
+    return new;
+}
+
+#endif
